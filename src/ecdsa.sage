2faa479379b1b053cd497ab0f9566accaa85bdea
kernoelpanic <kernoelpanic@3-volution.net>
2023-11-21 11:03:39 +0100

    Added introductory notebook (and slides) to ECC with some example code snippets

---
+++ src/ecdsa.sage
@@ -0,0 +1,60 @@
+#!/usr/bin/sage
+# inspired by
+# https://asecuritysite.com/sage/sage_04
+
+import hashlib
+
+# Secp256k1 https://www.secg.org/sec2-v2.pdf
+p  = 2**256-2**32-2**9 -2**8 - 2**7 - 2**6 - 2**4 - 1
+Fp = FiniteField(p)
+a  = 0
+b  = 7
+E  = EllipticCurve(Fp, [a, b])
+G  = E((55066263022277343669578718895168534326250603453777594175500187360389116729240, 
+32670510020758816978083085130507043184471273380659243275938904335757337482424))
+n  = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+h  = 1
+Fn = FiniteField(n)
+
+def ecdsa_hashtoint(msg):	
+  return Integer('0x' + hashlib.sha256(msg.encode()).hexdigest())
+
+def ecdsa_keygen(verbose=False):
+  d = randint(1, n - 1)
+  Q = d * G
+  if verbose: print(f"d   = {hex(d)}\nQ_x = {hex(Q.xy()[0])}\nQ_y = {hex(Q.xy()[1])}")
+  return (Q, d)
+
+def ecdsa_sign(d, m, nonce=None, verbose=False):
+  r = 0
+  s = 0
+  while s == 0:
+    while r == 0:
+      if nonce:
+        k = nonce
+      else:
+        k = randint(1, n - 1)
+      R = k * G
+      (x1, y1) = R.xy()
+      r = Fn(x1)
+    e = Fn(m) # assume hashed message
+    #e = ecdsa_hashtoint(m)
+    s = Fn(k) ^ (-1) * (Fn(e) + Fn(d) * Fn(r))
+  if verbose: print(f"k   = {hex(k)}\nG_x = {hex(G.xy()[0])}\nG_y = {hex(G.xy()[1])}\ne   = {hex(e)}\nr   = {hex(r)}\ns   = {hex(s)}")
+  return (r, s)
+
+def ecdsa_verify(Q, m, r, s, verbose=False):
+  #e = ecdsa_hashtoint(m)
+  e = Fn(m) # assume hashed message
+  w = Fn(s) ^ (-1)
+  u1 = (e * w)
+  u2 = (r * w)
+  P1 = Integer(u1) * G
+  P2 = Integer(u2) * Q
+  P = P1 + P2
+  (x, y) = P.xy()
+  p_x = Fn(x)
+  if verbose: print(f"w   = {hex(w)}\ne   = {hex(e)}\nu1  = {hex(u1)}\nu2  = {hex(u2)}\nr   = {hex(r)}\np_x = {hex(p_x)}")
+  return p_x == r
+
+
---
+++ src/test_ecdsa.py
@@ -0,0 +1,59 @@
+""" pytest script for ecdsa.sage 
+
+This script imports all from sagemath
+and loads ecdsa.sage to test it using pytest
+
+$ python3 -m pytest test_ecdsa.p
+"""
+
+from sage.all import *
+
+load("./ecdsa.sage")
+
+def test_sagemath_import():
+    F=GF(5)
+    assert len(GF(5).list()) == 5,"Sage math import did not work properly"
+
+def test_ecdsa_sign_and_verify():
+    # test sign and verify 
+    (Q_A,d_A) = ecdsa_keygen(verbose=False)
+    message_hash = ecdsa_hashtoint("some string")
+    (r,s) = ecdsa_sign(d_A,message_hash,verbose=False)
+    assert True == ecdsa_verify(Q_A, message_hash, r, s, verbose=False)
+    message_hash = ecdsa_hashtoint("other message")
+    assert False == ecdsa_verify(Q_A, message_hash, r, s, verbose=False)
+
+def test_ecdsa_sign():
+    # a test vector from https://crypto.stackexchange.com/questions/41316/complete-set-of-test-vectors-for-ecdsa-secp256k1
+    d = 0xebb2c082fd7727890a28ac82f6bdf97bad8de9f5d7c9028692de1a255cad3e0f
+    k = 0x49a0d7b786ec9cde0d0721d72804befd06571c974b191efb42ecf322ba9ddd9a
+    sighash = 0x4b688df40bcedbe641ddb16ff0a1842d9c67ea1c3bf63f3e0471baa664531d1a
+    r_expected = 0x241097efbf8b63bf145c8961dbdf10c310efbb3b2676bbc0f8b08505c9e2f795
+    s_expected = 0x021006b7838609339e8b415a7f9acb1b661828131aef1ecbc7955dfb01f3ca0e
+
+    (r,s) = ecdsa_sign(d,sighash,nonce=k,verbose=False)
+    assert (r,s) == (r_expected,s_expected),"Wrong signature"
+
+def test_ecdsa_verify():
+    # a test vector from https://crypto.stackexchange.com/questions/41316/complete-set-of-test-vectors-for-ecdsa-secp256k1
+    sighash = 0x4b688df40bcedbe641ddb16ff0a1842d9c67ea1c3bf63f3e0471baa664531d1a
+    r = 0x241097efbf8b63bf145c8961dbdf10c310efbb3b2676bbc0f8b08505c9e2f795
+    s = 0x021006b7838609339e8b415a7f9acb1b661828131aef1ecbc7955dfb01f3ca0e
+    x = 0x779dd197a5df977ed2cf6cb31d82d43328b790dc6b3b7d4437a427bd5847dfcd
+    y = 0xe94b724a555b6d017bb7607c3e3281daf5b1699d6ef4124975c9237b917d426f
+    Q_A = E((x,y))
+    assert ecdsa_verify(Q_A, sighash, r, s, verbose=False)
+
+def test_secp256k1_trace():
+    assert p+1 - E.order() == E.trace_of_frobenius() == 432420386565659656852420866390673177327,"Wrong trace for secp256k1"
+
+def test_secp256k1_mul():
+    k = 2
+    x = 0xC6047F9441ED7D6D3045406E95C07CD85C778E4B8CEF3CA7ABAC09B95C709EE5
+    y = 0x1AE168FEA63DC339A3C58419466CEAEEF7F632653266D0E1236431A950CFE52A
+    
+    P = k*G
+    (P_x,P_y)=P.xy()
+    assert x == P_x and y == P_y,"k*G does not match expected (x,y)"
+    
+
